a67d63cac7282bb8e3c5000a39cf952d4448eedb
RaY8118 <parthghadi43@gmail.com>
2026-02-06 20:45:35 +0530

    Initial Commit

---
+++ cli/lib/search_utils.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATA_PATH = PROJECT_ROOT/'data/movies.json'
+
+
+def load_movies():
+    with open(DATA_PATH, "r") as f:
+        data = json.load(f)
+
+    return data["movies"]
