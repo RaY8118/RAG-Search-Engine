f32b51e8f66dd40ff063018e861f43a43b1ee4a6
RaY8118 <parthghadi43@gmail.com>
2026-02-07 15:34:58 +0530

    feat: Implement keyword search with stemming and stopword removal
    - Added text cleaning, tokenization, stemming, and stopword removal to improve search accuracy.
    - Updated  to use new text processing utilities.
    - Modified  to load stopwords.
    - Added  as a dependency in  and updated .
    - Added .vscode directory

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,13 +1,48 @@
-from lib.search_utils import load_movies
+from lib.search_utils import load_movies, load_stopwords
+from nltk.stem import PorterStemmer
+import string
+
+stemmer = PorterStemmer()
+
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
+def tokenize_text(text):
+    text = clean_text(text)
+    stopwords = load_stopwords()
+    res = []
+
+    def _filter(tok):
+        if tok and tok not in stopwords:
+            return True
+        return False
+    for tok in text.split():
+        if _filter(tok):
+            tok = stemmer.stem(tok)
+            res.append(tok)
+
+    return res
+
+
+def has_matching_token(query_tokens, movie_tokens):
+    for query_tok in query_tokens:
+        for movie_tok in movie_tokens:
+            if query_tok in movie_tok:
+                return True
+    return False
 
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query in movie["title"]:
+        movie_tokens = tokenize_text(movie['title'])
+        if has_matching_token(query_tokens, movie_tokens):
             res.append(movie)
-
         if len(res) == n_results:
             break
 
