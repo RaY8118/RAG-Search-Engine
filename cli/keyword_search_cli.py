dbbd75210442582bb320b9a2d56e150f8d15a14a
RaY8118 <parthghadi43@gmail.com>
2026-02-07 22:16:03 +0530

    feat: Implement keyword search and TF calculation
    This commit introduces a new keyword search functionality to the CLI.
    
    Key changes include:
    - Added  command to create an inverted index, document map, and term frequencies cache.
    - Added  command to retrieve term frequency for a given document ID and term.
    - Integrated   for  in the  command to ensure correct type handling.
    - Implemented  class for managing index, docmap, and term frequencies.
    - Added  and  utilities for text processing.
    - Configured VS Code settings for automatic Black formatting and import organization with isort.

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,15 +1,23 @@
 import argparse
-from lib.keyword_search import search_command
+
+from lib.keyword_search import build_command, search_command, tf_command
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
-    subparsers = parser.add_subparsers(
-        dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser(
-        "search", help="Search movies using BM25 algorithm")
+        "search", help="Search movies using BM25 algorithm"
+    )
+
     search_parser.add_argument("query", help="Search query")
+
+    search_parser = subparsers.add_parser("build", help="Build")
+
+    search_parser = subparsers.add_parser("tf")
+    search_parser.add_argument("doc_id", type=int, help="Document ID for to check")
+    search_parser.add_argument("term", help="Search term to find counts for")
 
     args = parser.parse_args()
 
@@ -19,6 +27,11 @@ def main() -> None:
             results = search_command(args.query, 5)
             for i, result in enumerate(results):
                 print(f"{i} {result['title']}")
+
+        case "build":
+            build_command()
+        case "tf":
+            tf_command(args.doc_id, args.term)
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,8 +1,79 @@
-from lib.search_utils import load_movies, load_stopwords
-from nltk.stem import PorterStemmer
+import os
+import pickle
 import string
+from collections import Counter, defaultdict
+
+from lib.search_utils import CACHE_PATH, load_movies, load_stopwords
+from nltk.stem import PorterStemmer
 
 stemmer = PorterStemmer()
+
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = defaultdict(set)
+        self.index_path = CACHE_PATH / "index.pkl"
+        self.docmap = {}
+        self.docmap_path = CACHE_PATH / "docmap.pkl"
+        self.term_frequencies = defaultdict(Counter)
+        self.term_frequencies_path = CACHE_PATH / "term_frequencies.pkl"
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize_text(text)
+        for token in set(tokens):
+            self.index[token].add(doc_id)
+        self.term_frequencies[doc_id].update(tokens)
+
+    def get_documents(self, term):
+        return sorted(list(self.index[term]))
+
+    def get_tf(self, doc_id, term):
+        token = tokenize_text(term)
+        if len(token) != 1:
+            raise ValueError("Can only have 1 token")
+        return self.term_frequencies[doc_id][token[0]]
+
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            doc_id = movie["id"]
+            text = f"{movie["title"]} {movie["description"]}"
+            self.__add_document(doc_id, text)
+            self.docmap[doc_id] = movie
+
+    def save(self):
+        os.makedirs(CACHE_PATH, exist_ok=True)
+        with open(self.index_path, "wb") as f:
+            pickle.dump(self.index, f)
+
+        with open(self.docmap_path, "wb") as f:
+            pickle.dump(self.docmap, f)
+
+        with open(self.term_frequencies_path, "wb") as f:
+            pickle.dump(self.term_frequencies, f)
+
+    def load(self):
+        with open(self.index_path, "rb") as f:
+            self.index = pickle.load(f)
+
+        with open(self.docmap_path, "rb") as f:
+            self.docmap = pickle.load(f)
+
+        with open(self.term_frequencies_path, "rb") as f:
+            self.term_frequencies = pickle.load(f)
+
+
+def tf_command(doc_id, term):
+    idx = InvertedIndex()
+    idx.load()
+    print(idx.get_tf(doc_id, term))
+
+
+def build_command():
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
 
 def clean_text(text):
     text = text.lower()
@@ -19,6 +90,7 @@ def tokenize_text(text):
         if tok and tok not in stopwords:
             return True
         return False
+
     for tok in text.split():
         if _filter(tok):
             tok = stemmer.stem(tok)
@@ -35,15 +107,21 @@ def has_matching_token(query_tokens, movie_tokens):
     return False
 
 
-def search_command(query, n_results):
+def search_command(query, n_results=5):
     movies = load_movies()
-    res = []
+    idx = InvertedIndex()
+    idx.load()
+    seen, res = set(), []
     query_tokens = tokenize_text(query)
-    for movie in movies:
-        movie_tokens = tokenize_text(movie['title'])
-        if has_matching_token(query_tokens, movie_tokens):
-            res.append(movie)
-        if len(res) == n_results:
-            break
 
+    for qt in query_tokens:
+        matching_doc_ids = idx.get_documents(qt)
+        for matching_doc_id in matching_doc_ids:
+            if matching_doc_id in seen:
+                continue
+            seen.add(matching_doc_id)
+            matching_doc = idx.docmap[matching_doc_id]
+            res.append(matching_doc)
+            if len(res) >= n_results:
+                return res
     return res
